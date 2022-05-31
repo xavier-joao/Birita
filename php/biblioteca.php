df535eb222b17cc1278f9bba3b44e6e537b915f4
vaxier-bsi <joaopx@yahoo.com.br>
2022-05-31 20:16:23 -0300

    iniciado biblioteca estilo e php

---
+++ php/biblioteca.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <!-- BOOTSTRAP CSS E JS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <!--CSS CUSTOMIZADO-->
+    <link rel="stylesheet" type="text/css" href="css/styleBiblioteca.css">
+    
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-3">
+                    <span class="filter-heading">Ingredientes:</span>
+
+                </div>
+            </div>
+        </div>
+    </body>
+
+
+
+
+</html>
