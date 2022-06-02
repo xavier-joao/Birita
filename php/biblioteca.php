309bbfb2ae81fdc6c332030cfc9127de4a7cbd84
vaxier-bsi <joaopx@yahoo.com.br>
2022-06-01 22:33:50 -0300

    Biblioteca funcionando, basta adicionar filtros

--- php/biblioteca.php
+++ php/biblioteca.php
@@ -1,4 +1,7 @@
 <!DOCTYPE html>
+<?php
+include("fetchBiblioteca.php"); 
+?>
 <html>
     <head>
     <!-- BOOTSTRAP CSS E JS -->
@@ -7,15 +10,68 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <!--CSS CUSTOMIZADO-->
-    <link rel="stylesheet" type="text/css" href="css/styleBiblioteca.css">
+    <link rel="stylesheet" type="text/css" href="../css/styleBiblioteca.css">
     
     </head>
     <body>
         <div class="container">
             <div class="row">
                 <div class="col-sm-3">
-                    <span class="filter-heading">Ingredientes:</span>
+                    <span class="filter-heading">Drincoteca:</span>
+                    <input type="text" class="form-control" id="live_search" autocomplete="off" placeholder="Busque seus ingredientes aqui!">
+                </div>
+                <script type="text/javascript">
+                    $(document).ready(function(){
+                        $("#live_search").keyup(function(){
+                            var input = $(this).val();
 
+                            if(input != ""){
+                                $.ajax({
+                                    url:"livesearch.php",
+                                    method:"POST",
+                                    data:{input:input},
+
+                                    success:function(data){
+                                        $("#searchResults").html(data);
+                                    }
+                                });
+                            
+                            }else{
+                                $("#searchResults").css("display","none");
+                            }
+                        })
+                    })
+                </script>
+            </div>
+            <div class="col-sm-9">
+                <div id="searchResults" class="search-results-block">
+                    <div class="row">
+                        <?php
+                        while($row = mysqli_fetch_assoc($r)){
+                            $nomereceita =  $row['nomereceita']; 
+                            $tagsreceita =  $row['tagsreceita']; 
+                            $descricaoreceita =  $row['descricaoreceita']; 
+                            $imagemreceita =  $row['imagemreceita']; 
+                        ?>
+                        <div class="col-sm-4">
+                            <div class="product-card">
+                                <div class="card">
+                                    <img src="../img/<?php echo $imagemreceita;?>" style="width: 100%">
+                                        <h1><?php echo $nomereceita;?></h1>
+                                        <p class="desc"><?php echo $descricaoreceita;?></p>
+                                    <div class="row">
+                                        
+                                            <p class="tags"><?php echo $tagsreceita;?></p>
+                    
+                                    </div>
+                                    <p><button>Favoritar</button></p>
+                                </div>
+                            </div>
+                        </div>
+                        <?php
+                        }
+                        ?>
+                    </div>
                 </div>
             </div>
         </div>
