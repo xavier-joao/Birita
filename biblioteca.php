4c1819ffcb113a23477d803f316fec1e8408fc31
vaxier-bsi <joaopx@yahoo.com.br>
2022-06-11 00:05:51 -0300

    correçao biblioteca

--- biblioteca.php
+++ biblioteca.php
@@ -15,27 +15,18 @@ include("php/fetchBiblioteca.php");
     </head>
     <body>
 <div class="container">
-
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a href="index.html"><img src="img/logo.png" class="logo"></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="indexLogin.html">Login</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="indexCadastro.html">Cadastro</a>
-            </li>
-            </ul>
-        </div>
-    </nav>
+    <div class="navbar">
+      <a href="index.html"><img src="img/logo.png" class="logo"></a>
+      <nav>
+        <ul id="menuList">
+          <li><a href="biblioteca.php">Drincoteca</a></li>
+          <li><a href="indexLogin.html">Login</a></li>
+          <li><a href="indexCadastro.html">Cadastro</a></li>
+        </ul>
+      </nav>
+      <img src="img/menu.png" class="menu-icon" onclick="togglemenu()">
+    </div>
+  
         <div class="row d-flex justify-content-center">
             <div class="col-sm-3">
                 <span class="filter-heading d-flex justify-content-center">Drincoteca</span>
@@ -51,41 +42,43 @@ include("php/fetchBiblioteca.php");
                 </div>
             </div>
         </div>
-        <div class="col-sm-9">
-                <div id="searchResults" class="search-results-block">
-                    <div class="row">
-                        <?php
-                        while($row = mysqli_fetch_assoc($r)){
-                            $nomereceita =  $row['nomereceita']; 
-                            $tagsreceita =  $row['tagsreceita']; 
-                            $descricaoreceita =  $row['descricaoreceita']; 
-                            $imagemreceita =  $row['imagemreceita']; 
-                        ?>
-                        <div class="col-sm-4">
-                            <div class="product-card">
-                                <div class="card">
-                                    <img src="img/<?php echo $imagemreceita;?>" style="width: 100%">
-                                        <h1><?php echo $nomereceita;?></h1>
-                                        <button class="botaoReceita">Saiba como fazer</button>
+        <div class="row justify-content-center">
+            <div class="col-sm-9">
+                    <div id="searchResults" class="search-results-block">
+                        <div class="row">
+                            <?php
+                            while($row = mysqli_fetch_assoc($r)){
+                                $nomereceita =  $row['nomereceita']; 
+                                $tagsreceita =  $row['tagsreceita']; 
+                                $descricaoreceita =  $row['descricaoreceita']; 
+                                $imagemreceita =  $row['imagemreceita']; 
+                            ?>
+                            <div class="col-sm-4">
+                                <div class="product-card">
+                                    <div class="card">
+                                        <img src="img/<?php echo $imagemreceita;?>" style="width: 100%">
+                                            <h1><?php echo $nomereceita;?></h1>
+                                            <button class="botaoReceita">Saiba como fazer</button>
 
+                                            <div class="row">
+                                            <p class="desc"><?php echo $descricaoreceita;?></p>
+                                            </div>
                                         <div class="row">
-                                        <p class="desc"><?php echo $descricaoreceita;?></p>
+                                            <p class="tags"><?php echo $tagsreceita;?></p>
                                         </div>
-                                    <div class="row">
-                                        <p class="tags"><?php echo $tagsreceita;?></p>
+                                        <p><button>Favoritar</button></p>
                                     </div>
-                                    <p><button>Favoritar</button></p>
                                 </div>
                             </div>
+                            <?php
+                            }
+                            ?>
                         </div>
-                        <?php
-                        }
-                        ?>
                     </div>
                 </div>
             </div>
         </div>
-</div>
+    </div>
 
     </body>
 
