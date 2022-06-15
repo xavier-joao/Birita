afad19933318addddf1fcd62bb332773d5726001
vaxier-bsi <joaopx@yahoo.com.br>
2022-06-15 18:31:49 -0300

    limpeza logout

--- php/logout.php
+++ php/logout.php
@@ -3,13 +3,7 @@
     session_start();
 
     unset($_SESSION["loggedIn"]);
-    
-    //header('Location: index.php');
-
     $obj["logged"] = "false";
-
-    //echo("<script>location.href('index.html');</script>");
-
     echo json_encode($obj);
 
 ?>
